9c3e2fc39c639b71931313bd12c7218f549f7f49
ext.qi.chen1 <ext.qi.chen1@jd.com>
2023-06-16 14:28:18 -0700

    init project again

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -6,14 +6,14 @@ import (
 )
 
 type jsonResponse struct {
-	Error   bool   `json:"error"`
+	Error bool `json:"error"`
 	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
+	Data any `json:"data,omitempty"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
-		Error:   false,
+		Error: false,
 		Message: "Hit the broker",
 	}
 
